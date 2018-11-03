bd7af5327c456a6f4d5cdc10b63695d71094c354
James McCleese <jmccleese@gmail.com>
2018-11-03 06:27:43 +0000

    Add CheatController

---
+++ app/Http/Controllers/CheatController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cheat;
+use App\Http\Resources\CheatResource;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class CheatController extends Controller
+{
+    public function index()
+    {
+        return CheatResource::collection(Cheat::all());
+    }
+
+    public function store(Request $request)
+    {
+        $cheat = Cheat::create($request->cheat);
+        if (!$cheat) {
+            return JsonResponse(
+                null,
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+        return new CheatResource($cheat);
+    }
+
+    public function show(Cheat $cheat)
+    {
+        return new CheatResource($cheat);
+    }
+
+    public function update(Request $request, Cheat $cheat)
+    {
+        $cheat->update($request->cheat);
+        return new CheatResource($cheat);
+    }
+
+    public function destroy(Cheat $cheat)
+    {
+        if ($cheat->delete()) {
+            return new JsonResponse(
+                null,
+                Response::HTTP_NO_CONTENT
+            );
+        } else {
+            return new JsonResponse(
+                null,
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+}
