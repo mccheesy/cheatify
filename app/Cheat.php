b394cd66eda0f4e74c71c6169ce4d3f753c6abbc
James McCleese <jmccleese@gmail.com>
2018-11-03 04:12:06 +0000

    Add Cheat model

---
+++ app/Cheat.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use App\Traits\HasUuid;
+use Illuminate\Database\Eloquent\Model;
+use Webpatser\Uuid\Uuid;
+
+class Cheat extends Model
+{
+    use HasUuid;
+
+    protected $fillable = [
+        'name', 'description', 'code', 'status', 'creator_id',
+    ];
+
+    protected $hidden = [
+        'id',
+    ];
+
+    public function creator()
+    {
+        return $this->belongsTo('App\User', 'creator_id');
+    }
+}
