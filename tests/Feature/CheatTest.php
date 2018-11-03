c1ce63da69684c43c3f68105b8cca678dd2b67da
James McCleese <jmccleese@gmail.com>
2018-11-03 06:26:08 +0000

    Create test for Cheat

---
+++ tests/Feature/CheatTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Http\Response;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CheatTest extends TestCase
+{
+    use WithFaker;
+
+    /** @test **/
+    public function a_user_can_create_cheats()
+    
+{        $this->artisan('migrate');
+
+        $user = factory('App\User')->create();
+        $cheat = [
+            'name' => $this->faker->sentence(6, true),
+            'description' => $this->faker->paragraph(6, true),
+            'code' => $this->faker->bothify('????????'),
+            'creator_id' => $user->id,
+        ];
+
+        $response = $this->actingAs($user)
+                         ->json('POST', '/api/cheats', ['cheat' => $cheat]);
+
+        $response->assertStatus(Response::HTTP_CREATED);
+    }
+}
