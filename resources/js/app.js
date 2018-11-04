6c4d3a2a2736c623527ee9e10861f8ea92a0b097
James McCleese <jmccleese@gmail.com>
2018-11-04 03:14:24 +0000

    Rename CheatComponent to CheatShow

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap';
 import router from './routes';
 
 Vue.component('cheatify-navbar', require('./components/CheatifyNavbar.vue'));
-Vue.component('cheat-component', require('./components/CheatComponent.vue'));
+Vue.component('cheat-show', require('./components/CheatShow.vue'));
 
 const app = new Vue({
     el: '#app',
---
+++ resources/js/components/CheatShow.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-card>
+        <b-media>
+            <h5>
+                <strong>{{ this.cheat.name }}</strong>
+                <small class="text-muted">{{ this.cheat.code }}</small>
+            </h5>
+            <p>{{ this.cheat.description }}</p>
+            <p class="text-right">@{{ this.cheat.creator.name }}</p>
+        </b-media>
+    </b-card>
+</template>
+
+<script>
+export default {
+    props: [
+        'cheat',
+    ]
+}
+</script>
