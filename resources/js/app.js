1dcbeff88ca3e47a2ee4c1d97b018765b1e91c15
James McCleese <jmccleese@gmail.com>
2018-11-04 05:48:27 +0000

    Add Laravel Passport components

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,10 @@ import router from './routes';
 Vue.component('cheatify-navbar', require('./components/CheatifyNavbar.vue'));
 Vue.component('cheat-show', require('./components/CheatShow.vue'));
 
+Vue.component('passport-clients', require('./components/passport/Clients.vue'));
+Vue.component('passport-authorized-clients', require('./components/passport/AuthorizedClients.vue'));
+Vue.component('passport-personal-access-tokens', require('./components/passport/PersonalAccessTokens.vue'));
+
 const app = new Vue({
     el: '#app',
     router
