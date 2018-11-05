8156d248a312671cb59568c3f0ac93242ad040f8
James McCleese <jmccleese@gmail.com>
2018-11-04 23:37:12 -0600

    Add edit button if logged in

--- resources/js/components/CheatsShow.vue
+++ resources/js/components/CheatsShow.vue
@@ -2,17 +2,31 @@
     <b-card>
         <b-media>
             <h5>
-                <strong>{{ this.cheat.name }}</strong>
-                <small class="text-muted">{{ this.cheat.code }}</small>
+                <strong>{{ cheat.name }}</strong>
+                <small class="text-muted">Code: {{ cheat.code }}</small>
             </h5>
-            <p>{{ this.cheat.description }}</p>
-            <p class="text-right">@{{ this.cheat.creator.name }}</p>
+            <p>{{ cheat.description }}</p>
+            <p class="text-right">
+                @{{ cheat.creator.name }}
+                <b-btn
+                    @click="$emit('edit', cheat)"
+                    size="sm"
+                    variant="outline-info"
+                    v-if="is_auth">
+                    Edit
+                </b-btn>
+            </p>
         </b-media>
     </b-card>
 </template>
 
 <script>
 export default {
+    computed: {
+        is_auth() {
+            return window.is_auth;
+        }
+    },
     props: [
         'cheat',
     ]
