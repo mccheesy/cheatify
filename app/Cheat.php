35524f18a7963611d5d737bfca48c208da103d91
James D McCleese <jmccleese@gmail.com>
2018-11-05 23:59:39 -0600

    Add Global Scope to Cheat to sort desc by created_at

--- app/Cheat.php
+++ app/Cheat.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\Traits\HasUuid;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Webpatser\Uuid\Uuid;
 
@@ -21,6 +22,15 @@ class Cheat extends Model
     protected $with = [
         'creator',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        // default order in desc by created_at
+        static::addGlobalScope('latest_first', function (Builder $builder) {
+            $builder->orderByDesc('created_at');
+        });
+    }
 
     public function creator()
     {
